b51a22d127aaf76aca0753cd761dc31b8da6c52d
lufias69 <syaifulbachrimustamin@gmail.com>
2020-02-27 08:12:36 +0700

    Update AES.py

--- koding/aes_class/AES.py
+++ koding/aes_class/AES.py
@@ -38,8 +38,68 @@ def stopwords_list(list_kalimat):
     for i in list_kalimat:
         hasil_stopwords.append(stopwords_removal(i))
     return hasil_stopwords
-	
 
+
+import requests
+import json
+# from modulku import StemNstopW as stm
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def save(data, nama):
+    with open(dir_path+'/data/'+nama, 'w') as f:
+    # with open('data/'+nama, 'w') as f:
+        json.dump(data, f)
+        
+def get_value_dict(dict_):
+    value_ = list()
+    for _, val in dict_.items():
+        value_.append(val)
+    return value_
+
+def get_data(name):
+    with open(dir_path+'/data/'+str(name), "r") as filename:
+        return json.load(filename)
+
+kateglo = get_data('kateglo.json')
+
+def fs(cek):
+    try:
+        list_objek = requests.get('http://kateglo.com/api.php?format=json&phrase='+str(cek)).json()['kateglo']['relation']['s']
+        keranjang = dict()
+        for _, isi in list_objek.items():
+            try:
+                keranjang.update({isi['related_phrase']:cek})
+            except:
+                pass
+        return keranjang
+    except:
+        return {}
+
+def find_sinonim(fitur):
+    dict_all = get_data("sinonim_kateglo.json")
+    list_kata_ada = set(get_value_dict(dict_all))
+    if type(fitur) is not list:
+        return "fitur harus bertype list"
+    
+    
+    for i in fitur:
+        if i not in list_kata_ada:
+            x = fs(i)
+            dict_all.update(x)
+    save(dict_all, "sinonim_kateglo.json")
+    return dict_all 
+
+def qe(s, fitur):
+    dict_sinonim = find_sinonim(fitur)
+#     print(dict_sinonim)
+    s = s.split()
+    for ix, i in enumerate(s):
+        if i not in fitur and i in dict_sinonim:
+            s[ix]=dict_sinonim[i]
+    return " ".join(s) 
+
+#qe end
 def square_rooted(x):
     return sqrt(sum([a * a for a in x]))
 def cosine_similarity(x, y):
@@ -70,7 +130,7 @@ def get_unique_words(list_words):
     
 
 class AES:
-    def __init__(self, tf_idf=False, tf=False, binary=False,stop_words=True, ngram=False, n=2, stemming=True, toleransi=0.95):
+    def __init__(self, tf_idf=False, tf=False, binary=False,stop_words=True, ngram=False, n=2, stemming=True, toleransi=0.95, qe = False):
         self.tf_idf = tf_idf
         self.tf = tf
         self.binary = binary
@@ -79,9 +139,11 @@ class AES:
         self.stemming = stemming
         self.toleransi = toleransi
         self.stop_words = stop_words
+        self.qe = qe
         
     def train(self, kunci_jawaban, label):
         self.kata_unik = get_unique_words(kunci_jawaban)
+        # self.fitur_awal = get_unique_words(kunci_jawaban)
         
         if self.stemming:
             kunci_jawaban = stemming_list(kunci_jawaban)
@@ -141,8 +203,13 @@ class AES:
     def predict(self, jawaban):
         jawaban = jawaban.lower()
         self.jawaban = typo(jawaban, self.kata_unik, self.toleransi)
+
+        if self.qe:
+            self.jawaban = qe(jawaban, self.kata_unik)
+
         if self.stemming:
             jawaban = stemming_kalimat(self.jawaban)
+            
         self.weight_jawaban = self.model_w.transform([self.jawaban]).A[0]
         
         self.hasil_cosine_similarity = list()
